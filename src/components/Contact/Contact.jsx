f56ca09971d3df8a0a0fbaad333964fa68a7a0d1
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-10-22 07:31:25 +0530

    css improvement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <section className="form-section w-full flex flex-col items-center py-10 mt-20 px-24">
+    <section className="form-section w-full flex flex-col items-center py-10 mt-20 md:px-24 px-2">
       <h2 className="text-2xl font-semibold py-5">JOIN HYDRA</h2>
       <h3 className="text-2xl font-thin py-5">
         Let’s Build Your VR Experience
@@ -17,7 +17,7 @@ const Contact = () => {
           <input type="text" name="phone" id="phone" placeholder="Phone Number" className="form-inputs md:w-1/2 w-full" />
         </div>
         <input type="text" name="subject" id="subject" placeholder="Subject" className="form-inputs col-span-12 md:w-auto w-full" />
-        <textarea name="message" id="message" placeholder="Tell Us Something..." className="form-inputs col-span-12 md:w-auto w-full"></textarea>
+        <textarea name="message" id="message" placeholder="Tell Us Something..." className="form-inputs col-span-12 md:w-auto w-full h-52 py-4"></textarea>
         <div className="w-full flex items-center justify-center col-span-12">
           <button className="btn px-8 py-2 rounded-full mx-auto">SEND TO HYDRA</button>
         </div>
